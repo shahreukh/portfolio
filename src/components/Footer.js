59828d5e93b241352aa71559e85f1dd76b7b1b54
Shahrukh Mirza Nawandish <shahreukh@gmail.com>
2025-05-11 10:26:02 +0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -20,7 +20,7 @@ export const Footer = () => {
               <a href="https://github.com/shahreukh" target="_blank"><img src={navIcon2} alt="Icon" /></a>
               <a href="https://www.instagram.com/shahreukh/" target="_blank"><img src={navIcon3} alt="Icon" /></a>
             </div>
-            <p>Copyright 2022. All Rights Reserved</p>
+            <p>Copyright {new Date().getFullYear()}. All Rights Reserved</p>
           </Col>
         </Row>
       </Container>
